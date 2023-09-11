c221e0098eb17599e2e488f2424d5818cb5f077a
Vincent <mrccniv@gmail.com>
2023-09-11 21:14:32 +0200

    remove email verif idea bc service cost

--- api/src/index.ts
+++ api/src/index.ts
@@ -3,6 +3,9 @@ import express from "express";
 import cors from "cors";
 import authRoutes from "./routes/authRoutes";
 import { authMiddleware } from "./middlewares/authMiddleware";
+import passport from "passport";
+import session from "express-session";
+import flash from "connect-flash";
 
 const prisma = new PrismaClient();
 const app = express();
@@ -10,6 +13,28 @@ const port = 3000;
 
 app.use(cors());
 app.use(express.json());
+
+app.use(
+  session({
+    secret: "your-secret-key", // Change this to a secure secret
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+  done(null, user);
+});
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Initialize connect-flash middleware
+app.use(flash());
 
 app.use(authMiddleware);
 
--- api/src/routes/authRoutes.ts
+++ api/src/routes/authRoutes.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import { body } from "express-validator";
-import { register, login } from "./authController";
+import { register, login } from "../controllers/authController";
 
 const router = express.Router();
 
