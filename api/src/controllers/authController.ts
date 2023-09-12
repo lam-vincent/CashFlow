0d5559f005ba5a026268cd2f3eaddb0901365a19
Vincent <mrccniv@gmail.com>
2023-09-12 23:59:25 +0200

    old auth routes

--- api/src/controllers/authController.ts
+++ api/src/controllers/authController.ts
@@ -1,78 +1,93 @@
+import express, { Request, Response } from "express";
 import bcrypt from "bcrypt";
-import jwt from "jsonwebtoken";
-import { Request, Response } from "express";
-import { validationResult } from "express-validator";
+import jwt, { Secret } from "jsonwebtoken";
 import { PrismaClient } from "@prisma/client";
+import { config } from "dotenv";
+
+config();
+const secretKey: Secret = process.env.SECRET_KEY || "";
 
 const prisma = new PrismaClient();
+const router = express.Router();
 
-// Register a new user
-export const register = async (req: Request, res: Response): Promise<void> => {
-  // Validate request data using express-validator
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() });
-  }
-
-  const { username, email, password } = req.body;
+router.post("/register", async (req: Request, res: Response) => {
+  const { username, password } = req.body;
 
   try {
-    // Check if the email is already in use
-    const existingUser = await prisma.user.findUnique({ where: { email } });
+    // Check if the username is already taken
+    const existingUser = await prisma.user.findUnique({ where: { username } });
     if (existingUser) {
-      return res.status(400).json({ message: "Email is already registered" });
+      return res.status(409).json({ message: "Username already exists" });
     }
 
     // Hash the password
-    const saltRounds = 10;
-    const hashedPassword = await bcrypt.hash(password, saltRounds);
+    const hashedPassword = await bcrypt.hash(password, 10);
 
-    // Create a new user
+    // Create the user
     const newUser = await prisma.user.create({
       data: {
         username,
-        email,
         password: hashedPassword,
       },
     });
 
-    // Generate a JWT token for the new user
-    const token = jwt.sign({ userId: newUser.id }, "your-secret-key", {
-      expiresIn: "1h", // Set the token expiration time
+    // Generate JWT token
+    const token = jwt.sign({ userId: newUser.id }, secretKey, {
+      expiresIn: "1h",
     });
 
-    res.status(201).json({ token });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: "Server error" });
-  }
-};
+    await prisma.taskList.create({
+      data: {
+        title: "Todo",
+        userId: newUser.id,
+      },
+    });
+    await prisma.taskList.create({
+      data: {
+        title: "In progress",
+        userId: newUser.id,
+      },
+    });
+    await prisma.taskList.create({
+      data: {
+        title: "Done",
+        userId: newUser.id,
+      },
+    });
 
-// Login a user
-export const login = async (req: Request, res: Response): Promise<void> => {
-  const { email, password } = req.body;
+    res.status(201).json({ message: "User registered successfully", token });
+  } catch (error) {
+    console.error("Registration failed:", error);
+    res.status(500).json({ message: "Registration failed" });
+  }
+});
+
+router.post("/login", async (req: Request, res: Response) => {
+  const { username, password } = req.body;
 
   try {
     // Check if the user exists
-    const user = await prisma.user.findUnique({ where: { email } });
+    const user = await prisma.user.findUnique({ where: { username } });
     if (!user) {
-      return res.status(400).json({ message: "Invalid credentials" });
+      return res.status(401).json({ message: "Invalid username or password" });
     }
 
-    // Compare the provided password with the hashed password
+    // Verify the password
     const isPasswordValid = await bcrypt.compare(password, user.password);
     if (!isPasswordValid) {
-      return res.status(400).json({ message: "Invalid credentials" });
+      return res.status(401).json({ message: "Invalid username or password" });
     }
 
-    // Generate a JWT token for the authenticated user
-    const token = jwt.sign({ userId: user.id }, "your-secret-key", {
-      expiresIn: "1h", // Set the token expiration time
+    // Generate JWT token
+    const token = jwt.sign({ userId: user.id }, secretKey, {
+      expiresIn: "1h",
     });
 
-    res.status(200).json({ token });
+    res.status(200).json({ message: "Login successful", token });
   } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: "Server error" });
+    console.error("Login failed:", error);
+    res.status(500).json({ message: "Login failed" });
   }
-};
+});
+
+export default router;
